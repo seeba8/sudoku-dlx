2783f8146a49d65b6c70557e24a2d265dfc1eaa2
Sebastian <ober.sebastian@googlemail.com>
2017-01-18 23:54:48 +0100

    solves empty sudoku, but not given one.

--- src/dlx.py
+++ src/dlx.py
@@ -21,7 +21,79 @@ class Header(Node):
         self.chead = self
 
     def __str__(self):
-        return str(self.v)
+        return str(self.name)
+
+
+class Matrix(list):
+    def __init__(self):
+        # see: http://www.stolaf.edu/people/hansonr/sudoku/exactcovermatrix.htm
+        # 81+81+81+81=324 columns:
+        # 1) Some number in cell x (only one value)
+        #    r1c1, r1c2, ..., r2c1, r2c2, ..., r9c8, r9c9
+        # 2) Number n must appear in row r
+        #    r1#1, r1#2, ..., r2#1, r2#2, ..., r9#8, r9#9
+        # 3) Number n must appear in column c
+        #    c1#1, c1#2, ..., c2#1, c2#2, ..., c9#8, c9#9
+        # 4) Number n must appear in block b
+        #    b1#1, b1#2, ..., b2#1, b2#2, ..., b9#8, b9#9
+        #    with b1=r1c1-r3c3, b2=r1c4-r3c6, ...
+        #
+        # 9*9*9=729 rows:
+        # Number n at position (x,y): Possibilities to set a number
+        # (r,c):n -->
+        # r1c1#1
+        # r1c2#2
+        # ...
+        # r1c2#1
+        # r1c2#2
+        # ...
+        # r9c9#8
+        # r9c9#9
+        list.__init__(self, [x[:] for x in [[0] * (81 * 4)] * 9 ** 3])
+        # list.__init__(self, [[0] * (81 + 81 + 81 + 81)] * (9 * 9 * 9))
+
+        for r in range(9):
+            for c in range(9):
+                for n in range(9):
+                    # every row has four 1s, one for row, one for column, one for cell, one for existance
+                    y = self.get_row_number(r, c, n)
+                    # print(y)
+                    for i in self.get_corresponding_constraints(r, c, n):
+                        self[y][i] = 1  # block constraint
+                        # print(sum(self[y]))
+        # self.names = [str(i) for i in range(9)] * 9 * 4
+        self.names = ["rc{}{}".format(r, c) for r in range(9) for c in range(9)]
+        self.names += ["r{}#{}".format(r, c) for r in range(9) for c in range(9)]
+        self.names += ["c{}#{}".format(r, c) for r in range(9) for c in range(9)]
+        self.names += ["b{}#{}".format(r, c) for r in range(9) for c in range(9)]
+        # input(self.names)
+        # print(self[1])
+
+    def get_row_number(self, r, c, n):
+        return 9 * 9 * r + 9 * c + n
+
+    def set_number_in_constraint(self, r, c, n):
+        """
+        We can remove one row per set number, as well as 4 columns (one for each type of constraint, that is,
+        one for each 1 in the row
+        Remove them or set them to zero. Set to zero causes problems maybe?
+        even the number is zero-based (sudoku from 0 to 8)
+        :param x:
+        :param y:
+        :param cell:
+        :return:
+        """
+        self[self.get_row_number(r, c, n)] = [0] * (4 * 81)
+        for y in range(len(self)):
+            for x in self.get_corresponding_constraints(r, c, n):
+                # print("({},{}".format(x,y))
+                self[y][x] = 0
+
+    def get_corresponding_constraints(self, r, c, n):
+        yield 0 * 81 + r * 9 + c  # cell constraint
+        yield 1 * 81 + r * 9 + n  # row constraint
+        yield 2 * 81 + c * 9 + n  # column constraint
+        yield 3 * 81 + 27 * (r // 3) + 9 * (c // 3) + n  # block constraint
 
 
 def main(matrix):
@@ -30,21 +102,56 @@ def main(matrix):
     :param matrix:
     :return:
     """
-    A = fill_matrix(matrix)
+    # m = get_constraint_matrix(matrix)
+    m = Matrix()
+    A = fill_matrix(m)
     # for row in A:
     #     for cell in row:
     #         print(cell.info())
     search(0)
 
 
+def get_constraint_matrix(sudoku):
+    matrix = Matrix()
+    for r in range(len(sudoku)):
+        row = sudoku[r]
+        for c in range(len(row)):
+            n = row[c]
+            if n != 0:
+                matrix.set_number_in_constraint(r, c, n - 1)
+    return matrix
+
+
 def print_solution():
+    sudoku = [x[:] for x in [[0] * 9] * 9]
     for key, val in o.items():
-        print(val.chead.name, end="")
-        r = val.right
-        while r != val:
-            print(r.chead.name, end="")
-            r = r.right
-        print()
+        r, c, v = 0, 0, 0
+        # print(val.chead.name, end="")
+        if val.chead.name.startswith("rc"):
+            r = int(val.chead.name[2])
+            c = int(val.chead.name[3])
+        elif val.chead.name.startswith("r"):
+            r = int(val.chead.name[1])
+            v = int(val.chead.name[3]) + 1
+        elif val.chead.name.startswith("c"):
+            c = int(val.chead.name[1])
+            v = int(val.chead.name[3]) + 1
+        right = val.right
+        while right != val:
+            # print(r.chead.name, end="")
+            if right.chead.name.startswith("rc"):
+                r = int(right.chead.name[2])
+                c = int(right.chead.name[3])
+            elif right.chead.name.startswith("r"):
+                r = int(right.chead.name[1])
+                v = int(right.chead.name[3]) + 1
+            elif right.chead.name.startswith("c"):
+                c = int(right.chead.name[1])
+                v = int(right.chead.name[3]) + 1
+            right = right.right
+        sudoku[r][c] = v
+    print("asd")
+    print(sudoku)
 
 
 def choose_column():
@@ -56,6 +163,8 @@ def choose_column():
             c = j
             s = j.size
         j = j.right
+    if c == None:
+        assert (False)
     return c.chead
 
 
@@ -91,6 +200,7 @@ def uncover_column(c):
 def search(k):
     h = A[0][-1]
     if h.right == h:
+        # print(k)
         print_solution()
         return
     else:
@@ -116,8 +226,8 @@ def search(k):
 
 
 def fill_matrix(matrix):
-    names = matrix[0]
-    matrix = matrix[1:]
+    names = matrix.names
+    # matrix = matrix[1:]
     headerrow = []
     left, leftmost = None, None
     h = None
@@ -145,6 +255,9 @@ def fill_matrix(matrix):
 
     for j in range(len(matrix)):
         row = matrix[j]
+        if sum(row) == 0:
+            print("SUMZERO")
+            continue
         left, leftmost = None, None
         A.append([])
         x = A[-1]
@@ -175,13 +288,24 @@ def fill_matrix(matrix):
 
 
 if __name__ == "__main__":
-    example = [["A", "B", "C", "D", "E", "F", "G"],
-               [0, 0, 1, 0, 1, 1, 0],
-               [1, 0, 0, 1, 0, 0, 1],
-               [0, 1, 1, 0, 0, 1, 0],
-               [1, 0, 0, 1, 0, 0, 0],
-               [0, 1, 0, 0, 0, 0, 1],
-               [0, 0, 0, 1, 1, 0, 1]]
+    # example = [["A", "B", "C", "D", "E", "F", "G"],
+    #            [0, 0, 1, 0, 1, 1, 0],
+    #            [1, 0, 0, 1, 0, 0, 1],
+    #            [0, 1, 1, 0, 0, 1, 0],
+    #            [1, 0, 0, 1, 0, 0, 0],
+    #            [0, 1, 0, 0, 0, 0, 1],
+    #            [0, 0, 0, 1, 1, 0, 1]]
+    e = ["500080049",
+         "000500030",
+         "067300001",
+         "150000000",
+         "000208000",
+         "000000018",
+         "700004150",
+         "030002000",
+         "490050003"]
+    example = [[int(r[y]) for y in range(9)] for r in e]
+    # print(example)
 
     A = []
     o = {}
